040cce61b98afc69026a11dcfc705771ec7fb958
Jagruti2004deore <deorejagruti284@gmail.com>
2026-04-02 11:16:49 +0530

    Swapping Element in Array

---
+++ Arrays/Swapping.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Swapping{
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int i = 1;
+        int j = 3;
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
