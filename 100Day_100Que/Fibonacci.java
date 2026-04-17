c079cece7afa3de30e7db310655bd30fd7a895a6
Jagruti2004deore <deorejagruti284@gmail.com>
2026-04-17 20:12:03 +0530

    DSA Practice

---
+++ 100Day_100Que/Fibonacci.java
@@ -0,0 +1,36 @@
+public class Fibonacci {
+    public static void main(String[]args){
+        int n = 7;
+
+        int result = fib(n);
+
+        System.out.println(result);
+
+    }
+
+    static int fib(int n){
+
+        if(n == 0){
+            return 0;
+        }else if (n == 1) {
+            return 1;
+        }
+
+        int firstTerm = 0;
+        int secondTerm = 1;
+
+        for (int i = 2; i<= n; i++){
+
+            int thirdTerm = firstTerm + secondTerm;
+
+            firstTerm = secondTerm;
+            secondTerm = thirdTerm;
+
+        }
+        return secondTerm;
+
+    
+    }
+
+    }
+
